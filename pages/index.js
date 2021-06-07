61d696328c61afff5c0797939c45082ee53993d2
Fuadit Muhammad <muhammadfuaditrockz@gmail.com>
2021-06-07 16:32:15 +0700

    Update

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Image from 'next/image'
+import { MdSearch, MdMenu } from 'react-icons/md'
 import { Container, Row, Col, Jumbotron, Button } from 'react-bootstrap'
 import styles from '../styles/Home.module.css'
 
@@ -6,17 +7,30 @@ export default function Home() {
   return (
     <Container fluid className={styles.MindhiveContainer}>
       <Row className={styles.MindhiveNavbar}>
-        <Col>
-          
+        <Col className={styles.MindhiveNavbar_left} xs={6}>
+          <img
+            src="/mh-helpcenter-logo.png"
+            alt="Mindhive Help Center Logo"
+            height={25}
+          />
+        </Col>
+        <Col className={styles.MindhiveNavbar_right} xs={6}>
+          <MdSearch color='#fff' size={20} />
         </Col>
       </Row>
       <Jumbotron fluid className={styles.MindhiveJumbotron}>
+        <h1>
+          Everything you need to know <br /> so you can use Mindhive:
+        </h1>
       </Jumbotron>
-      <Row>
-        <Col>
-        
-        </Col>
-      </Row>
+      <div className={styles.MindhiveMenu}>
+        <div className={styles.menu_icon}>
+          <MdMenu color='#000' size={20} />
+        </div>
+        <div className={styles.menu_text}>
+          <h4>Creating A Discussion</h4>
+        </div>
+      </div>
     </Container>
   )
 }
